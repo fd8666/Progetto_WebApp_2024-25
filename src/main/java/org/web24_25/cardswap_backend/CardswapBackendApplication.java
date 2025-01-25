92f82bb65c9f9cca07ae9326ae5f0aa09f7a5e5e
Wip-Sama <47224042+Wip-Sama@users.noreply.github.com>
2025-01-25 16:47:21 +0100

    backend

---
+++ src/main/java/org/web24_25/cardswap_backend/CardswapBackendApplication.java
@@ -0,0 +1,13 @@
+package org.web24_25.cardswap_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CardswapBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CardswapBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/web24_25/cardswap_backend/CardswapBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.web24_25.cardswap_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CardswapBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
