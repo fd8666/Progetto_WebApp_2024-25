3365e2579e81bfc6d34e0b4360cc81c52d3f46ad
FraFalbo <falbofrancescopio@gmail.com>
2025-01-31 09:43:02 +0100

    Creazione FrontPage Angular

---
+++ src/app/front-page/front-page.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {RouterModule} from '@angular/router';
+
+@Component({
+  selector: 'app-front-page',
+  imports: [CommonModule,RouterModule],
+  templateUrl: './front-page.component.html',
+  standalone: true,
+  styleUrl: './front-page.component.css'
+})
+export class FrontPageComponent {
+
+}
