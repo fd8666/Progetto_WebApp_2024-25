f746bbf1b987114a34c51d8f7a1c862e3b87d902
FraFalbo <falbofrancescopio@gmail.com>
2025-01-31 12:09:15 +0100

    Css ,JS ecc.

--- src/app/front-page/front-page.component.ts
+++ src/app/front-page/front-page.component.ts
@@ -1,13 +1,43 @@
-import { Component } from '@angular/core';
-import {CommonModule} from '@angular/common';
-import {RouterModule} from '@angular/router';
+import { Component, OnInit, Renderer2 } from '@angular/core';
+import {RouterLink} from '@angular/router';
 
 @Component({
   selector: 'app-front-page',
-  imports: [CommonModule,RouterModule],
   templateUrl: './front-page.component.html',
-  standalone: true,
-  styleUrl: './front-page.component.css'
-
+  styleUrls: ['./front-page.component.css'],
+  imports: [
+    RouterLink
+  ],
+  standalone: true
 })
-export class FrontPageComponent {}
+export class FrontPageComponent implements OnInit {
+
+  constructor(private renderer: Renderer2) {}
+
+  ngOnInit() {
+    const accountButton = this.renderer.selectRootElement('#accountButton', true);
+    const accountMenu = accountButton.nextElementSibling;
+
+    this.renderer.listen(accountButton, 'click', () => {
+      accountMenu.classList.toggle('show');
+    });
+
+    const cartButton = this.renderer.selectRootElement('#cartButton', true);
+    const cartMenu = cartButton.nextElementSibling;
+
+    this.renderer.listen(cartButton, 'click', () => {
+      cartMenu.classList.toggle('show');
+    });
+
+    this.renderer.listen(document, 'click', (event: Event) => {
+      if (!(event.target as HTMLElement).matches('#accountButton') &&
+        !(event.target as HTMLElement).matches('#cartButton')) {
+        document.querySelectorAll('.dropdown-menu').forEach(menu => {
+          if (menu.classList.contains('show')) {
+            menu.classList.remove('show');
+          }
+        });
+      }
+    });
+  }
+}
