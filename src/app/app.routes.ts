c8630a66f5b286424860a5f599afd109669c6ada
fd8666 <fabiodelio4@gmail.com>
2025-02-01 10:35:57 +0100

    annuncio carta

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,13 @@ import { Routes } from '@angular/router';
 import {FrontPageComponent} from './front-page/front-page.component';
 import {RegistrazioneComponent} from './registrazione/registrazione.component';
 import {LoginComponent} from './login/login.component';
+import {ProfiloComponent} from './profilo/profilo.component';
+import {InfoCartaComponent} from './info-carta/info-carta.component';
 
 export const routes: Routes = [
   {path:'',component:FrontPageComponent},
   {path:'Registrazione', component:RegistrazioneComponent},
-  {path:'Login',component:LoginComponent}
+  {path:'Login',component:LoginComponent},
+  {path:'profilo',component: ProfiloComponent},
+  {path:'info-carta',component:InfoCartaComponent}
 ];
---
+++ src/app/info-carta/info-carta.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-info-carta',
+  imports: [],
+  templateUrl: './info-carta.component.html',
+  styleUrl: './info-carta.component.css'
+})
+export class InfoCartaComponent {
+
+}
