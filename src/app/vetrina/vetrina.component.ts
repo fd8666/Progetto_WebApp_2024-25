72754479a063ff0eda5966e2e822627d3c9acf34
FraFalbo <falbofrancescopio@gmail.com>
2025-02-02 11:04:36 +0100

    Component Vetrina

---
+++ src/app/vetrina/vetrina.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-vetrina',
+  imports: [],
+  templateUrl: './vetrina.component.html',
+  styleUrl: './vetrina.component.css'
+})
+export class VetrinaComponent {
+
+}
